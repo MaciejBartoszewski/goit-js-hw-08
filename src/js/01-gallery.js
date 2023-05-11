e8ca015626ba0050fcb51997defc38ddb0b78c6d
Maciej Bartoszewski <bartoszewski.maciek@gmail.com>
2023-05-11 23:48:11 +0200

    commit hw8 check

---
+++ src/js/01-gallery.js
@@ -0,0 +1,54 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import simpleLightbox from 'simplelightbox';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryList = document.querySelector('.gallery');
+
+function createGallery(images) {
+  return images
+    .map(
+      image =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+        <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+         />
+         </a>
+    </div>`
+    )
+    .join('');
+}
+
+const addGallery = createGallery(galleryItems);
+galleryList.innerHTML = addGallery;
+
+console.log(galleryList);
+
+galleryList.addEventListener('click', onImageClick);
+
+const instance = new simpleLightbox('.gallery a');
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  instance.on('show.simplelightbox');
+
+  galleryList.addEventListener('keydown', offEscapeClose);
+
+  function offEscapeClose(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+      galleryList.removeEventListener('keydown', offEscapeClose);
+    }
+  }
+}
